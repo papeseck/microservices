2e37b6260d420a77b9ad68322b722978c98ae54e
papeseck <diabel1208@gmail.com>
2024-01-12 14:37:25 +0100

    microservices

---
+++ api-gateway/src/main/java/com/groupeisi/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.groupeisi.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableHystrix
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	//Bean de la gateway
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
+}
